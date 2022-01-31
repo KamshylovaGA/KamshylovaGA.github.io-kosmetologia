b020aff1d437beb9194a29506ac857fd286b6a25
KamshylovaGA <90753885+KamshylovaGA@users.noreply.github.com>
2022-01-31 14:43:15 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu_first'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+//Slider
+
+let offset = 0;
+    let slideIndex = 1;
+
+    const slides = document.querySelectorAll('.first_slide'),
+        slider = document.querySelector('.first_slider'),
+        prev = document.querySelector('.first_prev'),
+        next = document.querySelector('.first_next'),
+        
+        slidesWrapper = document.querySelector('.first_slider-wrapper'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector('.first_slider-inner');
+
+    
+    
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    
+
+    next.addEventListener('click', () => {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2); 
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        
+    });
+
+    
